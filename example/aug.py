e9582373f35f450d7b573432415789c84eec388d
bffntocffn <jinvk.to@gmail.com>
2025-09-18 02:24:23 +0900

    sync

--- example/aug.py
+++ example/aug.py
@@ -7,18 +7,20 @@ import numpy as np
 modal_list = ['omat24', 'mp_r2scan', 'matpes_r2scan', 'pet_mad']
 
 def get_csv(modal):
-    csv_file = open(f'{modal}.csv', 'w', buffeering = 1)
+    csv_file = open(f'{modal}.csv', 'w', buffering = 1)
     csv_file.write('idx,formula,sgn,xx,zz\n')
     head = f'/data2_1/jinvk/25_LTC/omni/{modal}/cond'
     for i in range(25):
         if i == 7:
             continue
-        data = h5py.File(f'{head}/kappa_{idx}.hdf5', 'r')[str(idx)]
+        file = h5py.File(f'{head}/kappa_{i}.hdf5', 'r')
+        data = file[str(i)]
         kappa = data['kappa_TOT_RTA']
-        formula = kappa.attrs['formula']
-        sgn = kappa.attrs['sgn']
+        formula = data.attrs['formula']
+        sgn = data.attrs['sgn']
         xx, zz = kappa[0][0][0], kappa[0][0][2]
-        csv_file.write(f'{idx},{formula},{sgn},{xx},{zz}\n')
+        file.close()
+        csv_file.write(f'{i},{formula},{sgn},{xx},{zz}\n')
     csv_file.close()
 
 if __name__ == '__main__':
